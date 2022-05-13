2d9afda0dfcaddd4a5f96a7ae57e7cef3a16a917
tomvieir <wevertonbvieira@gmail.com>
2022-05-12 21:19:42 -0300

    Add header inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+     
+    </div>
+  )
+}
+
+export default Home
