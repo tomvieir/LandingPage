d2a22caa1447b7d8f63f01588411797059f7a85d
tomvieir <wevertonbvieira@gmail.com>
2022-05-14 00:32:46 -0300

    add Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Homehero from '../components/Homehero'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-     
+
+      <Head>
+        <title>Home</title>
+
+
+      </Head>
+
+      <Homehero />
+      
     </div>
   )
 }
