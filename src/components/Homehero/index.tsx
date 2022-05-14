6bf20936e4ed47f369128f3bb803e0a4414ce41d
tomvieir <wevertonbvieira@gmail.com>
2022-05-14 00:33:28 -0300

    Add componente Inicial Homehero

---
+++ src/components/Homehero/index.tsx
@@ -0,0 +1,45 @@
+import styles from './Homehero.module.scss'
+
+export default function Homehero() {
+    return (
+        <div className={styles.container}>
+            <img src='./../../personimg.jpg' alt="imagem" />
+
+            <div className={styles.rightContainer}>
+                <strong>Bem vindo!</strong>
+                <h2>Me chamo Weverton</h2>
+
+                <div className={styles.containerBox}>
+
+                    <div className={styles.boxContent}>
+                        <div className={styles.codeItem} >
+                            <span className={styles.comment}>//Minha apresentação</span><br />
+                            <span className={styles.obj}>Infos</span> {'\u007B'}
+                            <div className={styles.blue}>
+                                Nome: <span className={styles.string}>'Weverton',</span>
+                            </div>
+                            <div className={styles.blue}>
+                                Sobrenome: <span className={styles.string}>'Vieira'</span>
+                            </div>
+                            {'\u007D'}
+                        </div>
+                        <div className={styles.codeItem}>
+                            <span className={styles.obj}>Cargo</span> {'\u007B'}
+                            <div className={styles.blue}>
+                                Função: <span className={styles.string}>'Dev Front-end',</span>
+                            </div>
+                            <div className={styles.blue}>
+                                Empresa: <span className={styles.darkblue}>Null</span>
+                            </div>
+                            {'\u007D'}
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
