dbd26946f15fd8d294e055a1198be4dc2a31b6cf
tomvieir <wevertonbvieira@gmail.com>
2022-05-17 20:40:22 -0300

    Mudanças nos tamanhos das divs

--- src/components/Experiencias/index.tsx
+++ src/components/Experiencias/index.tsx
@@ -5,7 +5,10 @@ import styles from './Experiencias.module.scss'
 export default function Experiencias() {
     return (
         <div className={styles.container}>
-            <SectionTitle title="Experiência" description="#04 anos" />
+            <div className={styles.titleContainer}>
+                <SectionTitle title="Experiência" description="#04 anos" />
+            </div>
+            
             <section className={styles.itemContainer}>
                 <ExperienciaItem year="2020" title="Desenvolvedor Fullstack" description="Desenvolvimento de aplicações web e mobile." />
                 <ExperienciaItem year="2020" title="Desenvolvedor Fullstack" description="Desenvolvimento de aplicações web e mobile." />
