2015d17a976ad34d27babbf33ad5f9c24f063697
tomvieir <wevertonbvieira@gmail.com>
2022-05-24 20:52:13 -0300

    Button removido

--- src/components/Projetos/ProjetoItem/index.tsx
+++ src/components/Projetos/ProjetoItem/index.tsx
@@ -1,39 +1,38 @@
 import Link from "next/link";
-import { BsArrowRightCircle } from "react-icons/bs";
 import ButtonLearnMore from "../../ButtonLearnMore";
 import styles from './ProjetoItem.module.scss';
 
 interface ProjetoProps {
-    title: string;
-    type: string;
-    slug: string;
-    img: string;
-  }
-  
-  export default function ProjetoItem({ title, type, slug, img }: ProjetoProps) {
-    return (
-      <div className={styles.container}>
-          
-        <section>
-            <div>
-                
-            </div>
-          <img src={img} alt="img" />
-          <div className={styles.text}>
-            <h1># {title}</h1>
-            <h2>- {type}</h2>
-          </div>
-        </section>
-        <button type="button">
-          <Link href={`/projetos/${slug}`}>
-            <a>
-              <ButtonLearnMore />
-            </a>
-          </Link>
-        </button>
-      
+  title: string;
+  type: string;
+  slug: string;
+  img: string;
+}
 
-        
-      </div>
-    );
-  }
+export default function ProjetoItem({ title, type, slug, img }: ProjetoProps) {
+  return (
+    <div className={styles.container}>
+
+      <section>
+        <div>
+
+        </div>
+        <img src={img} alt="img" />
+        <div className={styles.text}>
+          <h1># {title}</h1>
+          <h2>- {type}</h2>
+        </div>
+      </section>
+      <button type="button">
+        <Link href={`/projetos/${slug}`}>
+          <a>
+            <ButtonLearnMore />
+          </a>
+        </Link>
+      </button>
+
+
+
+    </div>
+  );
+}
