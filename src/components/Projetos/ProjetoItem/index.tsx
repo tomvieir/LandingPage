1c3054523730af494de8bf18b22e7064a7ed89e0
tomvieir <wevertonbvieira@gmail.com>
2022-05-23 21:47:27 -0300

    Estilização das previas dos projetos e responsivo

--- src/components/Projetos/ProjetoItem/index.tsx
+++ src/components/Projetos/ProjetoItem/index.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import styles from './ProjetoItem.module.scss';
 
-
-export default function ProjetoItem() {
-    return(
-       <></> 
-    )
-}
+interface ProjetoProps {
+    title: string;
+    type: string;
+    slug: string;
+    img: string;
+  }
+  
+  export default function ProjetoItem({ title, type, slug, img }: ProjetoProps) {
+    return (
+      <div className={styles.container}>
+          
+        <section>
+            <div>
+                
+            </div>
+          <img src={img} alt="img" />
+          <div className={styles.text}>
+            <h1># {title}</h1>
+            <h2>- {type}</h2>
+          </div>
+        </section>
+        <button type="button">
+          <Link href={`/projetos/${slug}`}>
+            <a>
+              Ver mais 
+            </a>
+          </Link>
+        </button>
+      </div>
+    );
+  }
--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,19 +1,60 @@
 import Link from "next/link";
 import SectionTitle from "../SectionTitle";
+import ProjetoItem from "./ProjetoItem";
 import styles from './Projetos.module.scss';
 
 
-export default function Projetos() {
+interface IProjeto {
+    slug: string;
+    title: string;
+    type: string;
+    description: string;
+    link: string;
+    thumbnail: string;
+  }
+  
+  interface ProjetosProps {
+    projetos: IProjeto[];
+  }
+
+export default function Projetos({ projetos }: ProjetosProps) {
     return (
         <div className={styles.container}>
             <div className={styles.title}>
                 <SectionTitle title='#Ultimos ptojetos' description='' />
             </div>
-            <section>
-        
-          
-       
+
+            <section>           
+                <ProjetoItem
+                    key='##'
+                    img='./../../personimg.jpg'
+                    title='projeto 1'
+                    type='##'
+                    slug='##'
+                />
+                <ProjetoItem
+                    key='##'
+                    img='./../../personimg.jpg'
+                    title='projeto 1'
+                    type='##'
+                    slug='##'
+                />
+                <ProjetoItem
+                    key='##'
+                    img='./../../personimg.jpg'
+                    title='projeto 1'
+                    type='##'
+                    slug='##'
+                />
+                <ProjetoItem
+                    key='##'
+                    img='./../../personimg.jpg'
+                    title='projeto 1'
+                    type='##'
+                    slug='##'
+                />
             </section>
+
             <button type="button">
                 <Link href="/projetos">
                 <a>Ver todos os projetos</a>
