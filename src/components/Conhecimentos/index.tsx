58e909c4dc48079fb707bc46023fabda496eb4dc
tomvieir <wevertonbvieira@gmail.com>
2022-06-01 21:07:45 -0300

    aos aplicado nos itens

--- src/components/Conhecimentos/index.tsx
+++ src/components/Conhecimentos/index.tsx
@@ -1,6 +1,6 @@
 import {AiFillHtml5} from 'react-icons/ai';
 import {DiCss3, DiSass, DiJavascript1, DiReact, DiPython} from 'react-icons/di';
-import {SiTypescript, SiNextdotjs, SiMaterialui, SiStyledcomponents } from 'react-icons/si';
+import {SiTypescript, SiNextdotjs, SiMaterialui, SiStyledcomponents, SiChakraui } from 'react-icons/si';
 import {BsFillBootstrapFill} from 'react-icons/bs';
 import SectionTitle from '../SectionTitle';
 import styles from './Conhecimentos.module.scss';
@@ -15,57 +15,62 @@ export default function Conhecimentos() {
 
            
             <div className={styles.Itemscontainer}>
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>html</strong>
                     <AiFillHtml5/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
                     <strong>Css</strong>
                     <DiCss3/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>SASS</strong>
                     <DiSass/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
                     <strong>Javascript</strong>
                     <DiJavascript1/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>Typescript</strong>
                     <SiTypescript/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
                     <strong>ReactJs</strong>
                     <DiReact/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>NextJs</strong>
                     <SiNextdotjs/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
                     <strong>Bootstrap</strong>
                     <BsFillBootstrapFill/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>MaterialUi</strong>
                     <SiMaterialui/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
+                    <strong>ChakaraUI</strong>
+                    <SiChakraui/>
+                </div>
+
+                <div className={styles.item} data-aos='fade-left'>
                     <strong>Styled-Components</strong>
                     <SiStyledcomponents/>
                 </div>
 
-                <div className={styles.item}>
+                <div className={styles.item} data-aos='fade-right'>
                     <strong>Python</strong>
                     <DiPython/>
                 </div>
