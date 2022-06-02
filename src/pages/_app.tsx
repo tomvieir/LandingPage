782550f9dd6c1a31fd3eafc21618d29ceda1a6d7
tomvieir <wevertonbvieira@gmail.com>
2022-06-01 21:11:01 -0300

    footer add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 
 
@@ -12,7 +13,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 
       <Header />
       <Component {...pageProps} />
-   
+      <Footer />
+
     </>
   )
 }
