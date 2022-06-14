ccdbccb24931101a5f19bc0917d137f2cce3ef37
tomvieir <wevertonbvieira@gmail.com>
2022-06-14 17:17:02 -0300

    Feat: contador de caractere + validador do button

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -4,8 +4,20 @@ import { IoLogoWhatsapp } from 'react-icons/io';
 import swal from 'sweetalert';
 
 import styles from './Contato.module.scss';
+import { useCallback, useState } from "react";
 
 export default function Contato() {
+
+    const [characterCount, setCharacterCount] = useState(0);
+
+    const value = characterCount > 0 ? false : true
+
+    const handleCharacter = useCallback(
+        (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const len = event.target.value.length;
+        setCharacterCount(len); 
+    }
+    , []);
 
     const handleSwal = () => {
         swal({
@@ -21,14 +33,15 @@ export default function Contato() {
             />
 
             <div className={styles.formContainer}>
-                <form onClick={handleSwal}>
-                    <div className={styles.formGroup}>
-                        <input type="text" id="nome" placeholder="Nome" />
-                        <input type="email" id="email" placeholder="E-mail" />
+                <form >
+                    <div className={styles.formGroup}  >
+                        <input type="text" id="nome" placeholder="Nome" required />
+                        <input type="email" id="email" placeholder="E-mail" required />
                     </div>
-                    <textarea id="mensagem" placeholder="Mensagem" />
+                    <textarea id="mensagem" placeholder="Mensagem" onChange={handleCharacter} />
+                    <p>{characterCount}</p>
                 </form>
-                <button type="button">
+                <button type="button" disabled={value} onClick={handleSwal} >
                     <a>Enviar </a>
                 </button>
             </div>
